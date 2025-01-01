a87bb11e54e3612d9cedf2e65e20a58ad20b24d5
DanielStivenRivera <danestiv0610@gmail.com>
2025-01-01 07:28:11 -0500

    feat: server
    api and test finished

--- app-server/app-server/Program.cs
+++ app-server/app-server/Program.cs
@@ -6,45 +6,80 @@ using app_server.Infrastructure.Persistence;
 using app_server.Infrastructure.Persistence.Repositories;
 using app_server.Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+
+
         var builder = WebApplication.CreateBuilder(args);
 
-        // Connect to the database
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+
+
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
         {
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
-        
+
         builder.Services.AddScoped<UserRepository>();
         builder.Services.AddScoped<JwtTokenGenerator>();
         builder.Services.AddScoped<UserService>();
         builder.Services.AddScoped<UserAdapter>();
-        
         builder.Services.AddScoped<ReservationRepository>();
         builder.Services.AddScoped<ReservationService>();
         builder.Services.AddScoped<ReservationAdapter>();
-        
-        
+
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+
         app.UseSwagger();
         app.UseSwaggerUI();
         app.UseHttpsRedirection();
+        app.UseAuthentication();
         app.UseAuthorization();
+
+
+        app.Use(async (context, next) =>
+        {
+            if (!context.User.Identity?.IsAuthenticated ?? false)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized - No estás autenticado.");
+                return;
+            }
+
+            await next();
+        });
+
         app.MapControllers();
 
         app.UseMiddleware<ExceptionMiddleware>();
 
-        // Migrate the database
+
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
@@ -53,10 +88,10 @@ public class Program
             Console.WriteLine("Cannot connect to the database.");
             return;
         }
+
         using (var scope2 = app.Services.CreateScope())
         {
             var dbContext2 = scope2.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
             if (!dbContext2.Database.CanConnectAsync().Result)
             {
                 Console.WriteLine("Cannot connect to the database.");
@@ -80,10 +115,11 @@ public class Program
                 dbContext.SaveChanges();
             }
         }
-        
+
         AddDefaultSpaces(dbContext);
 
         app.Run();
+
     }
 
     private static void AddDefaultSpaces(ApplicationDbContext dbContext)
